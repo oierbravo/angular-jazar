a3aca4bf3526b3ccf8d0211bb22da50d38513155
Oier Bravo Urtasun <oierbravo@gmail.com>
2016-05-08 15:13:54 +0200

    v0.3

--- README.md
+++ README.md
@@ -3,17 +3,6 @@
 ###Installation
 	bower install angular-jazar --save
 ###Methods:
-#### Jazar.getAll()
-Returns $promise with all the posts.
-	>success:{status:'ok',posts:Array}
-#### Jazar.getPage(page)
-Return $promise with 10 posts from given page(1,2,3...).
-    >success:{status:'ok'
-    posts:Array}
-#### Jazar.getPost(postId)
-Returns $promise with a post from given id
-    >success:{status:'ok'
-    post:Object}
 
 ##- - WORK IN PROGRESS - - 
 ## DEVELOPERS
--- src/angular-jazar/services/angularJazar.services.js
+++ src/angular-jazar/services/angularJazar.services.js
@@ -1,60 +1,22 @@
 /*angularJazar.services.js*/
 
-angular.module('angularJazar.services').factory('Jazar',['$resource',
+var baseUrl = 'http://jazar.org/wp-json/wp/v2/';
+
+angular.module('angularJazar.services').factory('JazarPosts',['$resource',
 	function($resource){
-    var baseUrl = 'http://jazar.org/wp-json/wp/v2';
-    var query;
-    return $resource('http://jazar.org/wp-json/wp/v2/posts/:id', {id:'@id'});
+	    return $resource(baseUrl + 'posts/:id', {id:'@id',per_page:100});
 		
 	}]
 );
-/* REST v2 API   /wp-json/wp/v2   http://v2.wp-api.org/
-/posts
-
-
-*/
-
-
-/* JSON API  https://es.wordpress.org/plugins/json-api/other_notes/
-get_recent_posts/?post_type=ai1ec_event
- info
- get_recent_posts
- get_posts
- get_post
- get_page
- get_date_posts
- get_category_posts
- get_tag_posts
- get_author_posts
- get_search_results
- get_date_index
- get_category_index
- get_tag_index
- get_author_index
- get_page_index
- get_nonce 
-
-*/
-/*angular.module('angularJazar.services').factory('Jazar',['$http',
-	function($http){
-    var baseUrl = 'http://jazar.org/?json=1';
-    var query;
-		return {
-			//Get all posts - Returns $promise
-		    getAll: function(){
-		      query = $http.get(baseUrl + '&count=-1');
-		      return query;
-		    },
-		    //Get paginated posts Count of 10 -Arg page_num (1,2,3...- Returns $promise
-			getPage: function(page){
-			  query = $http.get(baseUrl + '&page=' + page);
-			  return query;
-			},
-			//Get a post - Arg: postId - Returns $promise
-	        getPost: function(postId){
-	          query = $http.get(baseUrl + '&p=' + postId);
-	          return query;
-	        }
-		};
+angular.module('angularJazar.services').factory('JazarEspaciosComunes',['$resource',
+	function($resource){
+	    return $resource(baseUrl + 'espacios-comunes/:id', {id:'@id',per_page:30});
+		
 	}]
-);*/
+);
+angular.module('angularJazar.services').factory('JazarMedia',['$resource',
+	function($resource){
+    return $resource(baseUrl + 'media/:id', {id:'@id'});
+		
+	}]
+);
